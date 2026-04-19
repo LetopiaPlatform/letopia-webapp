8f8e28d1787db8baac4cefe655760445d532dbe2
Mohamed Raafat <mohamedraafat.engineer@gmail.com>
2026-04-19 23:11:07 +0200

    refactor(auth): memoize AuthContext value and stabilize methods

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import { loadFeatureFlags } from './lib/featureFlags.ts';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+loadFeatureFlags().then(() => {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+});
